f67896b620355684c28e4ed1947424abc71eec78
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-21 18:59:22 +0100

    getById

--- src/main/java/de/tekup/db/controllers/EmployeeCtrl.java
+++ src/main/java/de/tekup/db/controllers/EmployeeCtrl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class EmployeeCtrl {
 	@GetMapping("/employee/get")
 	public List<EmployeeEntity> getAllEmp(){
 		return empService.getAllEmployee();
+	}
+	
+	@GetMapping("/employee/get/{id}")
+	public EmployeeEntity getOneEmp(@PathVariable("id") int id){
+		return empService.getEmpById(id);
 	}
 
 }
--- src/main/java/de/tekup/db/services/EmployeeService.java
+++ src/main/java/de/tekup/db/services/EmployeeService.java
@@ -1,6 +1,8 @@
 package de.tekup.db.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,15 @@ public class EmployeeService {
 	
 	public List<EmployeeEntity> getAllEmployee(){
 		return empRepos.findAll();
+	}
+	
+	public EmployeeEntity getEmpById(int id) {
+		Optional<EmployeeEntity> opt = empRepos.findById(id);
+//		if(opt.isPresent())
+//			return opt.get();
+//		throw new NoSuchElementException("Employee with id is not found");
+		return opt
+	.orElseThrow(()-> new NoSuchElementException("Employee with id is not found"));
 	}
 
 }
