54a169978d4b1ee1fc2b512ac794275889ef80b3
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-21 19:15:39 +0100

    handle exception

--- src/main/java/de/tekup/db/controllers/EmployeeCtrl.java
+++ src/main/java/de/tekup/db/controllers/EmployeeCtrl.java
@@ -1,8 +1,12 @@
 package de.tekup.db.controllers;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +34,12 @@ public class EmployeeCtrl {
 	@GetMapping("/employee/get/{id}")
 	public EmployeeEntity getOneEmp(@PathVariable("id") int id){
 		return empService.getEmpById(id);
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNSEE(NoSuchElementException e){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+							.body("Error in search : " + e.getMessage());
 	}
 
 }
